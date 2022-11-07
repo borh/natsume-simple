385360c478f810d48029f8f75a8abb8133a93904
Bor Hodošček <dev@bor.space>
2022-11-07 19:37:19 +0900

    fix: also unzip file in python script version

--- scripts/get-jnlp-corpus.py
+++ scripts/get-jnlp-corpus.py
@@ -2,13 +2,17 @@
 
 from pathlib import Path
 import urllib.request
-
+import zipfile
 
 if __name__ == "__main__":
-    file = Path("../data/NLP_LATEX_CORPUS.zip")
+    data_dir = Path("../data")
+    file = data_dir / "NLP_LATEX_CORPUS.zip"
     if not file.is_file():
         print("Downloading...")
         urllib.request.urlretrieve(
             "https://www.anlp.jp/resource/journal_latex/NLP_LATEX_CORPUS.zip",
             file,
         )
+
+    with zipfile.ZipFile(file, "r") as z:
+        z.extractall(data_dir)
