dc4a8779dcdfc8372494aa507fd7ab542936588a
Bor Hodošček <dev@bor.space>
2022-11-07 16:34:47 +0900

    feat: added tests and inital pdm enablement work

---
+++ tests/test_models.py
@@ -0,0 +1,11 @@
+import spacy
+
+def test_model_loading():
+    try:
+        nlp = spacy.load("ja_ginza_electra")
+    except Exception:
+        nlp = spacy.load("ja_ginza")
+
+    doc = nlp("これはテストです")
+    assert len(doc) == 4
+    assert [t.text for t in doc] == ["これ", "は", "テスト", "です"]
