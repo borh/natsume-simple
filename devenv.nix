f762fa2c3a685ed55e332aba8c78c8fdf23598bc
Bor Hodošček <dev@bor.space>
2023-10-18 22:31:43 +0900

    feat: initial rocm work

--- devenv.nix
+++ devenv.nix
@@ -1,6 +1,6 @@
 { pkgs, lib, ... }:
 let
-  cudaSupport = true;
+  cudaSupport = false;
   cudaVersion = "12.2";
   cuda-common-redist = with pkgs.cudaPackages_12_2; [
     cuda_cccl # cub/cub.cuh
@@ -10,7 +10,6 @@ let
     libcufft # cufft.h
     pkgs.cudaPackages_12_1.cudnn # cudnn.h # no 12.2 version
   ];
-
   cuda-native-redist = pkgs.symlinkJoin {
     name = "cuda-native-redist-${cudaVersion}";
     paths = with pkgs.cudaPackages_12_2; [
@@ -24,17 +23,61 @@ let
       pkgs.linuxPackages_latest.nvidia_x11
     ] ++ cuda-common-redist;
   };
-
   cuda-redist = pkgs.symlinkJoin {
     name = "cuda-redist-${cudaVersion}";
     paths = cuda-common-redist;
   };
+
+  rocmSupport = true;
+  rocmtoolkit_joined = pkgs.symlinkJoin {
+    name = "rocm-merged";
+    paths = with pkgs.rocmPackages; [
+      rocm-core
+      clr
+      rccl
+      miopen
+      miopengemm
+      rocrand
+      rocblas
+      rocsparse
+      hipsparse
+      rocthrust
+      rocprim
+      hipcub
+      roctracer
+      rocfft
+      rocsolver
+      hipfft
+      hipsolver
+      hipblas
+      rocminfo
+      rocm-thunk
+      rocm-comgr
+      rocm-device-libs
+      rocm-runtime
+      clr.icd
+      hipify
+    ];
+    # Fix `setuptools` not being found
+    postBuild = ''
+      rm -rf $out/nix-support
+    '';
+  };
+  native-deps = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    gcc-unwrapped.lib
+    stdenv
+    stdenv.cc
+    binutils
+  ];
 in
+lib.attrsets.recursiveUpdate
 {
   # https://devenv.sh/basics/
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.stdenv.cc.cc pkgs.zlib ] ++ lib.optionals cudaSupport [ cuda-native-redist ];
+  packages = native-deps ++ lib.optionals cudaSupport [ cuda-native-redist ] ++ lib.optionals rocmSupport [ rocmtoolkit_joined ];
 
   # https://devenv.sh/scripts/
 
@@ -60,10 +103,16 @@ in
 
   scripts.export-pip.exec = "poetry export -f requirements.txt | sed \"s/ ;.*//\" > requirements.txt; poetry export --extras cuda -f requirements.txt | sed \"s/ ;.*//\" > requirements-cuda.txt";
 
-  env.LD_LD_LIBRARY_PATH = lib.makeLibraryPath [ cuda-redist ];
-  env.CUDNN_HOME = "" + lib.optionals cudaSupport cuda-redist;
-  env.CUDA_HOME = "" + lib.optionals cudaSupport cuda-redist;
-  env.CUDA_PATH = "" + lib.optionals cudaSupport cuda-redist;
-
   # See full reference at https://devenv.sh/reference/options/
-}
+} # recursiveUpdate:
+  (if cudaSupport then {
+    env.LD_LIBRARY_PATH = lib.makeLibraryPath ([ cuda-redist ] ++ native-deps);
+    env.CUDNN_HOME = cuda-redist;
+    env.CUDA_HOME = cuda-redist;
+    env.CUDA_PATH = cuda-redist;
+  } else if rocmSupport then {
+    env.CUPY_INSTALL_USE_HIP = "1";
+    env.ROCM_HOME = rocmtoolkit_joined;
+    env.HCC_AMDGPU_TARGET = "gfx1100";
+    env.LD_LIBRARY_PATH = lib.makeLibraryPath ([ rocmtoolkit_joined ] ++ native-deps);
+  } else { })
