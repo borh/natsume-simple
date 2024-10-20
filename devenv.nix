e55717f08dd733ec8f1001a9cef4f360f2d6911b
Bor Hodošček <dev@bor.space>
2024-10-20 22:43:35 +0900

    chore: up and cleanup

--- devenv.nix
+++ devenv.nix
@@ -4,71 +4,70 @@
   config,
   ...
 }: let
-  cudaSupport = true;
-  cudaVersion = "12.2";
-  cuda-common-redist = with pkgs.cudaPackages_12_2; [
-    cuda_cccl # cub/cub.cuh
-    libcublas # cublas_v2.h
-    libcurand # curand.h
-    libcusparse # cusparse.h
-    libcufft # cufft.h
-    pkgs.cudaPackages_12_1.cudnn # cudnn.h # no 12.2 version
-  ];
-  cuda-native-redist = pkgs.symlinkJoin {
-    name = "cuda-native-redist-${cudaVersion}";
-    paths = with pkgs.cudaPackages_12_2;
-      [
-        cuda_cudart # cuda_runtime.h cuda_runtime_api.h
-        cuda_nvcc
-        cuda_nvtx
-        cuda_cupti
-        cuda_nvrtc
-        libcusolver
-        nccl
-        pkgs.linuxPackages_latest.nvidia_x11
-      ]
-      ++ cuda-common-redist;
-  };
-  cuda-redist = pkgs.symlinkJoin {
-    name = "cuda-redist-${cudaVersion}";
-    paths = cuda-native-redist;
-  };
-
-  rocmSupport = false;
-  rocmtoolkit_joined = pkgs.symlinkJoin {
-    name = "rocm-merged";
-    paths = with pkgs.rocmPackages; [
-      rocm-core
-      clr
-      rccl
-      miopen
-      miopengemm
-      rocrand
-      rocblas
-      rocsparse
-      hipsparse
-      rocthrust
-      rocprim
-      hipcub
-      roctracer
-      rocfft
-      rocsolver
-      hipfft
-      hipsolver
-      hipblas
-      rocminfo
-      rocm-thunk
-      rocm-comgr
-      rocm-device-libs
-      rocm-runtime
-      clr.icd
-      hipify
-    ];
-    # Fix `setuptools` not being found
-    postBuild = ''
-      rm -rf $out/nix-support
-    '';
-  };
+  #cudaSupport = false;
+  #cudaVersion = "12.4";
+  #cuda-common-redist = with pkgs.cudaPackages; [
+  #  cuda_cccl # cub/cub.cuh
+  #  libcublas # cublas_v2.h
+  #  libcurand # curand.h
+  #  libcusparse # cusparse.h
+  #  libcufft # cufft.h
+  #  cudnn # cudnn.h
+  #];
+  #cuda-native-redist = pkgs.symlinkJoin {
+  #  name = "cuda-native-redist-${cudaVersion}";
+  #  paths = with pkgs.cudaPackages;
+  #    [
+  #      cuda_cudart # cuda_runtime.h cuda_runtime_api.h
+  #      cuda_nvcc
+  #      cuda_nvtx
+  #      cuda_cupti
+  #      cuda_nvrtc
+  #      libcusolver
+  #      nccl
+  #      pkgs.linuxPackages_latest.nvidia_x11
+  #    ]
+  #    ++ cuda-common-redist;
+  #};
+  #cuda-redist = pkgs.symlinkJoin {
+  #  name = "cuda-redist-${cudaVersion}";
+  #  paths = cuda-native-redist;
+  #};
+  #rocmSupport = false;
+  #rocmtoolkit_joined = pkgs.symlinkJoin {
+  #  name = "rocm-merged";
+  #  paths = with pkgs.rocmPackages; [
+  #    rocm-core
+  #    clr
+  #    rccl
+  #    miopen
+  #    miopengemm
+  #    rocrand
+  #    rocblas
+  #    rocsparse
+  #    hipsparse
+  #    rocthrust
+  #    rocprim
+  #    hipcub
+  #    roctracer
+  #    rocfft
+  #    rocsolver
+  #    hipfft
+  #    hipsolver
+  #    hipblas
+  #    rocminfo
+  #    rocm-thunk
+  #    rocm-comgr
+  #    rocm-device-libs
+  #    rocm-runtime
+  #    clr.icd
+  #    hipify
+  #  ];
+  #  # Fix `setuptools` not being found
+  #  postBuild = ''
+  #    rm -rf $out/nix-support
+  #  '';
+  #};
   native-deps = with pkgs; [
     stdenv.cc.cc
     zlib
@@ -78,12 +77,15 @@
     binutils
   ];
 in
-  lib.attrsets.recursiveUpdate
+  # lib.attrsets.recursiveUpdate
   {
     # https://devenv.sh/basics/
 
     # https://devenv.sh/packages/
-    packages = native-deps ++ lib.optionals cudaSupport [cuda-native-redist] ++ lib.optionals rocmSupport [rocmtoolkit_joined];
+    packages =
+      native-deps
+      # ++ lib.optionals cudaSupport [cuda-native-redist] ++ lib.optionals rocmSupport [rocmtoolkit_joined]
+      ;
 
     # https://devenv.sh/scripts/
 
@@ -133,22 +135,23 @@ in
     '';
 
     # See full reference at https://devenv.sh/reference/options/
-  } # recursiveUpdate:
-  
-  (
-    if cudaSupport
-    then {
-      env.LD_LIBRARY_PATH = lib.makeLibraryPath ([cuda-redist] ++ native-deps);
-      env.CUDNN_HOME = cuda-redist;
-      env.CUDA_HOME = cuda-redist;
-      env.CUDA_PATH = cuda-redist;
-    }
-    else if rocmSupport
-    then {
-      env.CUPY_INSTALL_USE_HIP = "1";
-      env.ROCM_HOME = rocmtoolkit_joined;
-      env.HCC_AMDGPU_TARGET = "gfx1100";
-      env.LD_LIBRARY_PATH = lib.makeLibraryPath ([rocmtoolkit_joined] ++ native-deps);
-    }
-    else {}
-  )
+  }
+# recursiveUpdate:
+#(
+#    if cudaSupport
+#    then {
+#      env.LD_LIBRARY_PATH = lib.makeLibraryPath ([cuda-redist] ++ native-deps);
+#      env.CUDNN_HOME = cuda-redist;
+#      env.CUDA_HOME = cuda-redist;
+#      env.CUDA_PATH = cuda-redist;
+#    }
+#    else if rocmSupport
+#    then {
+#      env.CUPY_INSTALL_USE_HIP = "1";
+#      env.ROCM_HOME = rocmtoolkit_joined;
+#      env.HCC_AMDGPU_TARGET = "gfx1100";
+#      env.LD_LIBRARY_PATH = lib.makeLibraryPath ([rocmtoolkit_joined] ++ native-deps);
+#    }
+#    else {}
+#  )
+
