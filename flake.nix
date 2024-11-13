995ec6b1b779c8de602829fa9016016f23174076
Bor Hodošček <dev@bor.space>
2024-11-13 16:29:56 +0900

    fix: script->application

--- flake.nix
+++ flake.nix
@@ -26,7 +26,12 @@
         pkgs,
         system,
         ...
-      }: {
+      }: let
+        runtime-packages = [
+          pkgs.uv
+          pkgs.nodejs
+        ];
+      in {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
@@ -34,16 +39,16 @@
 
         devShells = {
           default = pkgs.mkShell {
-            nativeBuildInputs = [
-              pkgs.bashInteractive
-              pkgs.git
-              pkgs.nodejs
-              pkgs.bun
-              pkgs.wget
-              pkgs.pandoc
-              pkgs.sqlite
-              pkgs.uv
-            ];
+            nativeBuildInputs =
+              [
+                pkgs.bashInteractive
+                pkgs.git
+                pkgs.bun
+                pkgs.wget
+                pkgs.pandoc
+                pkgs.sqlite
+              ]
+              ++ runtime-packages;
             shellHook = ''
               # Set up Python and dependencies
               ${config.packages.initial-setup}/bin/initial-setup
@@ -55,31 +60,40 @@
           # TODO: Make backend, data, and frontend-specific devShells as well
         };
 
-        packages.initial-setup = pkgs.writeShellScriptBin "initial-setup" ''
-          PYTHON_VERSION=3.12.7
-          ${pkgs.uv}/bin/uv python install $PYTHON_VERSION
-          ${pkgs.uv}/bin/uv python pin $PYTHON_VERSION
-          ${pkgs.uv}/bin/uv sync --dev --extra backend
-        '';
-        packages.run-tests = pkgs.writeShellScriptBin "run-tests" ''
-          ${config.packages.initial-setup}/bin/initial-setup
-          uv run pytest
-        '';
-        packages.run-lint = pkgs.writeShellScriptBin "run-lint" ''
-          ${config.packages.initial-setup}/bin/initial-setup
-          uvx ruff format
-          uvx ruff check --output-format=github src notebooks tests
-          ${pkgs.mypy}/bin/mypy --ignore-missing-imports src
-        '';
-        packages.run-server = pkgs.writeShellApplication {
-          name = "run-server";
-          runtimeInputs = [
-            pkgs.uv
-            pkgs.nodejs
-          ];
+        packages.initial-setup = pkgs.writeShellApplication {
+          name = "initial-setup";
+          runtimeInputs = runtime-packages;
+          text = ''
+            PYTHON_VERSION=3.12.7
+            uv python install $PYTHON_VERSION
+            uv python pin $PYTHON_VERSION
+            uv sync --dev --extra backend
+          '';
+        };
+        packages.run-tests = pkgs.writeShellApplication {
+          name = "run-tests";
+          runtimeInputs = runtime-packages;
           text = ''
             ${config.packages.initial-setup}/bin/initial-setup
-            cd natsume-frontend && ${pkgs.nodejs}/bin/npm i && ${pkgs.nodejs}/bin/npm run build && cd ..
+            uv run pytest
+          '';
+        };
+        packages.run-lint = pkgs.writeShellApplication {
+          name = "run-lint";
+          runtimeInputs = runtime-packages;
+          text = ''
+            ${config.packages.initial-setup}/bin/initial-setup
+            uvx ruff format
+            uvx ruff check --output-format=github src notebooks tests
+            ${pkgs.mypy}/bin/mypy --ignore-missing-imports src
+          '';
+        };
+        packages.run-server = pkgs.writeShellApplication {
+          name = "run-server";
+          runtimeInputs = runtime-packages;
+          text = ''
+            ${config.packages.initial-setup}/bin/initial-setup
+            cd natsume-frontend && npm i && npm run build && cd ..
             uv run --with fastapi --with polars fastapi run --host localhost src/natsume_simple/server.py
           '';
         };
