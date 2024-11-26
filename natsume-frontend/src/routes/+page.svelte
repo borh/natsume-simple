b0b63deb3d51f94e2d96a41dd20e3bc783504783
Yo Cjyet <git@yocjyet.dev>
2024-11-26 16:49:40 +0900

    refactor: extract ThemeSwitch

--- natsume-frontend/src/routes/+page.svelte
+++ natsume-frontend/src/routes/+page.svelte
@@ -3,7 +3,8 @@
 	import Search from '$lib/Search.svelte';
 	import Options from '$lib/menus/Options.svelte';
 	import Stats from '$lib/menus/Stats.svelte';
-
+	import ThemeSwitch from '$lib/components/ThemeSwitch.svelte';
+	
 	import { onMount, tick, afterUpdate } from 'svelte';
 	import { writable, derived, type Writable } from 'svelte/store';
 	import './../tailwind.css';
@@ -586,12 +587,7 @@
 					</button>
 				</div>
 				{#if statsDropdownOpen}
-					<Stats
-						{corpusNorm}
-						{filteredResultCount}
-						{searchElapsedTime}
-						{formatNumber}
-					/>
+					<Stats {corpusNorm} {filteredResultCount} {searchElapsedTime} {formatNumber} />
 				{/if}
 				{#if optionsDropdownOpen}
 					<Options
@@ -605,42 +601,7 @@
 					/>
 				{/if}
 			</div>
-			<button
-				class="ml-2 p-1 rounded-full hover:bg-gray-200 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-				on:click={toggleDarkMode}
-			>
-				{#if $darkMode}
-					<svg
-						class="w-6 h-6 text-yellow-300"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-						></path>
-					</svg>
-				{:else}
-					<svg
-						class="w-6 h-6 text-gray-800"
-						fill="none"
-						stroke="currentColor"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-						></path>
-					</svg>
-				{/if}
-			</button>
+			<ThemeSwitch {toggleDarkMode} {darkMode} />
 		</div>
 	</header>
 
