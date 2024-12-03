e9882aaee608e7df6f2283833e54651f025fc21b
Yo Cjyet <git@yocjyet.dev>
2024-12-03 16:34:58 +0900

    chore: install unplugin-icons

--- natsume-frontend/vite.config.ts
+++ natsume-frontend/vite.config.ts
@@ -1,17 +1,23 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import Icons from "unplugin-icons/vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}']
-    },
-    build: {
-        sourcemap: true // Enable source maps
-    },
-    server: {
-        fs: {
-            allow: ['./tailwind.config.js']
-        }
-    }
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: "svelte",
+		}),
+	],
+	test: {
+		include: ["src/**/*.{test,spec}.{js,ts}"],
+	},
+	build: {
+		sourcemap: true, // Enable source maps
+	},
+	server: {
+		fs: {
+			allow: ["./tailwind.config.js"],
+		},
+	},
 });
